d35a5b3c5319d37c194aaac4db7d9283730cf157
siri <ksiric126@gmail.com>
2026-02-23 21:33:01 -0600

    commit

--- src/main/java/com/cinefy/chatappbackend/ChatAppBackendApplication.java
+++ src/main/java/com/cinefy/chatappbackend/ChatAppBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatAppBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatAppBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppBackendApplication.class, args);
+    }
 
 }
